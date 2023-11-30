db6461d019387a47faa2dfd252e1b6e3805c7630
NikolozK34 <kekelidze97@gmail.com>
2023-11-30 15:11:22 +0400

    Add project files.

---
+++ Davaleba3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace davaleba1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pirveli ricxbi: ");
+
+            double number1 = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("Meore ricxbi: ");
+
+            double number2 = Convert.ToDouble(Console.ReadLine());
+
+            double sum = number1 + number2;
+
+            bool isEven;
+
+            if(sum % 2 == 0)
+            {
+                isEven = true;
+                Console.WriteLine($"{sum} is even -> {isEven}");
+            }
+            else
+            {
+                isEven = false;
+                Console.WriteLine($"{sum} is even -> {isEven}");
+            }
+
+        }
+    }
+}
